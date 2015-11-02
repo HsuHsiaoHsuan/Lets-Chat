07343e664234a522d7a6c5487e89198d82b87d19
HsuHsiaoHsuan#CZ80 <hsuhsiaohsuan+cz80@gmail.com>
2015-11-02 17:08:42 +0800

    [2015/11/02]     1. update angular file.

--- chatnode.js
+++ chatnode.js
@@ -1,1 +1,47 @@
-angular.module('');
+angular.module('letsChatApp', []);
+
+angular.module('letsChatApp').factory('socket', function($rootScope) {
+  var socket = io.connect('/');
+  return {
+    on: function(eventName, callback) {
+      socket.on(eventName, function() {
+        var args = arguments;
+        $rootScope.$apply(function() {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function(eventName, data, callback) {
+      socket.emit(eventName, data, function() {
+        var args = arguments;
+        $rootScope.$apply(function() {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  }
+});
+
+angular.module('letsChatApp').controller('RoomCtrl', function($scope, socket) {
+  $scope.messages = [];
+  socket.emit('getAllMessages');
+  socket.on('allMessage', function(messages) {
+    $scope.messages = messages;
+  });
+  socket.on('messageAdded', function(message) {
+    $scope.messages.push(message);
+  });
+});
+
+angular.module('letsChatApp').controller('MessageCreatorCtrl', function($scope, socket) {
+  $scope.newMessage = '';
+  $scope.createMessage = function() {
+    if ($scope.newMessage == '') {
+      return;
+    }
+    socket.emit('createMessage', $scope.newMessage);
+    $scope.newMessage = '';
+  }
+});
