c5f71c491cd30132c57bcbb7a3ad942700727fd3
HsuHsiaoHsuan#CZ80 <hsuhsiaohsuan+cz80@gmail.com>
2015-11-03 16:56:51 +0800

    [2015/11/03]     1. temp

--- chatnode.js
+++ chatnode.js
@@ -1,6 +1,7 @@
-angular.module('letsChatApp', []);
+'use strict';
+var myApp = angular.module('letsChatApp', []);
 
-angular.module('letsChatApp').factory('socket', function($rootScope) {
+myApp.factory('socket', function($rootScope) {
   var socket = io.connect('/');
   return {
     on: function(eventName, callback) {
@@ -21,10 +22,10 @@ angular.module('letsChatApp').factory('socket', function($rootScope) {
         });
       });
     }
-  }
+  };
 });
 
-angular.module('letsChatApp').controller('RoomCtrl', function($scope, socket) {
+myApp.controller('RoomCtrl', function($scope, socket) {
   $scope.messages = [];
   socket.emit('getAllMessages');
   socket.on('allMessage', function(messages) {
@@ -35,7 +36,7 @@ angular.module('letsChatApp').controller('RoomCtrl', function($scope, socket) {
   });
 });
 
-angular.module('letsChatApp').controller('MessageCreatorCtrl', function($scope, socket) {
+myApp.controller('MessageCreatorCtrl', function($scope, socket) {
   $scope.newMessage = '';
   $scope.createMessage = function() {
     if ($scope.newMessage == '') {
@@ -44,4 +45,45 @@ angular.module('letsChatApp').controller('MessageCreatorCtrl', function($scope, 
     socket.emit('createMessage', $scope.newMessage);
     $scope.newMessage = '';
   }
+});
+
+myApp.directive('autoScrollToBottom', function() {
+  return {
+    link: function($scope, element, attrs) {
+      $scope.$watch(
+        function() {
+          return element.children().length;
+        },
+        function() {
+          element.animate({
+            scrollTop: element.prop('scrollHeight')
+          }, 1000);
+        }
+      );
+    }
+  };
+});
+
+angular.module('letsChatApp').directive('ctrlEnterBreakLine', function() {
+  return function($scope, element, attrs) {
+    var ctrlDown = false;
+    element.bind('keydown', function(evt) {
+      if(evt.which === 17) {
+        ctrlDown = true;
+        setTimeout(function() {
+          ctrlDown = false;
+        }, 1000);
+      }
+      if(evt.which === 13) {
+        if(ctrlDown) {
+          element.val(element.val() + '\n');
+        } else {
+          $scope.$apply(function() {
+            $scope.$eval(attrs.ctrlEnterBreakLine);
+          });
+          evt.preventDefault();
+        }
+      }
+    });
+  };
 });
